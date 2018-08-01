82cee1df09379900688265dba472daa336d9ee30
Victor <victor@MacBook-Pro-de-Victor.local>
2018-07-31 21:08:31 -0300

    criação da classe pages

--- index.php
+++ index.php
@@ -2,20 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-
-	$sql = new Hcode\DB\Sql();
-
-  $result = $sql->select("SELECT * FROM tb_users");
-
-  echo json_encode($result);
-
+$app->get('/', function () {
+    $page = new Hcode\Page();
+    $page->setTpl("index");
 });
 
 $app->run();
 
- ?>
+?>
